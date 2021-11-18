b50140acd7936ad4cefb338318666cef40964e62
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 13:33:43 +0100

    google api has been added

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useLocation} from 'react-router-dom';
+import ReactPlayer from "react-player";
 
+import {useResultContext} from "../../contexts/ResultContextProvider";
 
 
 const Results = () =>{
+    const { results, isLoading, getResults, searchTerm} = useResultContext();
+    const location = useLocation();
+
+    if(isLoading) return <Loading/>;
+
     return(
         <div>
             REsults
