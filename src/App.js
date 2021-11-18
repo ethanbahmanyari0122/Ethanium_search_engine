0e38a37744c2c7a2820abf18f2b4c496f2d455b7
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 12:07:59 +0100

    playing around with tailwind

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ const App = ()=>{
 
     return(
         <div className={darkTheme ? 'dark' : ''}>
-            <div className="bg-gray-100">
-                <h1>App</h1>
+            <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+                <Navbar/>
+                <Routes/>
+                <Footer/>
             </div>
         </div>
     );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = ()=>{
     return(
-        <div>
+        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
             <h1>Navbar</h1>
         </div>
     );
