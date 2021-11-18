304bcfa262d2def02a23707a03f9526f36d93601
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 12:13:15 +0100

    playing around with tailwind

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 const Navbar = ()=>{
     return(
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
-            <h1>Navbar</h1>
+            <div className="flex justify-between items-center space-x-5 w-screen">
+
+            </div>
         </div>
     );
 };
