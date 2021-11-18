314bf13131369b7c38548e73f08290a4a5bcfbf7
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 13:29:19 +0100

    google api has been added

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -24,4 +24,9 @@ export const ResultContextProvider = ({ children }) => {
         setResults(data);
         setIsLoading(false);
     }
+    return(
+        <ResultContext.Provider value={{getResults, results, searchTerm, setSearchTerm, isLoading}}>
+            {children}
+        </ResultContext.Provider>
+    )
 }
