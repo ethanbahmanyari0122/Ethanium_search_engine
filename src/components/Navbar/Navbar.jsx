4b579062dc8b04dcbdef48135c972f6d13ca7397
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 12:18:36 +0100

    playing around with tailwind

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = ()=>{
     return(
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
             <div className="flex justify-between items-center space-x-5 w-screen">
-
+                <Link to="/">
+                    <p className="text-2xl">
+                        Ethanium
+                    </p>
+                </Link>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDom from "react-dom"
+import ReactDom from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import './global.css';
 
-ReactDom.render(<App/>, document.getElementById('root'));
+ReactDom.render(<Router><App/></Router>, document.getElementById('root'));
