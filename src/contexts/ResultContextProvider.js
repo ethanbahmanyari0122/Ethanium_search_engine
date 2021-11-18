6a3159d55b991a649d41a0e4cc90820d398fa230
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 13:27:06 +0100

    google api has been added

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -8,4 +8,20 @@ const baseUrl = 'https://google-search1.p.rapidapi.com/google-search';
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState();
     const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    // /videos, /search, /images
+    const getResults = async (type) => {
+        setIsLoading( true);
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: 'Get',
+            headers: {
+                'x-rapidapi-host': 'google-search1.p.rapidapi.com',
+                'x-rapidapi-key': '472f58a7edmsha4265f5d786161dp1b5e3djsn89d3add5c915'
+            }
+        });
+        const data = await response.json();
+        setResults(data);
+        setIsLoading(false);
+    }
 }
