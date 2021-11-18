e8cb06a4d485f17af99359270ed58290051fee73
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 12:30:03 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = ()=>{
     return(
         <div className={darkTheme ? 'dark' : ''}>
             <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-                <Navbar/>
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
                 <Routes/>
                 <Footer/>
             </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = ()=>{
+const Navbar = ({darkTheme,setDarkTheme})=>{
     return(
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
             <div className="flex justify-between items-center space-x-5 w-screen">
                 <Link to="/">
-                    <p className="text-2xl">
-                        Ethanium
+                    <p className="text-2xl bg-blue-300 font-bold text-white py-1 px-2 rounded dark:bg-gray-300 dark:text-gray-900">
+                        Ethanium 👨🏻‍💻
                     </p>
                 </Link>
+                <button type="button" onClick={()=> setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-70 dark:text-gray-800 bg-blue-200 rounded-full px-2 py-1 hover:shadow-lg ">
+                    {darkTheme? "Light 🌕": "Dark 🌑"}
+                </button>
             </div>
         </div>
     );
