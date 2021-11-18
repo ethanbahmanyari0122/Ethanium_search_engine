6ad5ecd2d82a55e53646fba77a360bd913224ed9
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-18 12:01:11 +0100

    playing around with tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Routes from "./components/Routes/Routes";
@@ -6,9 +6,13 @@ import Routes from "./components/Routes/Routes";
 
 
 const App = ()=>{
+    const [darkTheme, setDarkTheme] = useState(false);
+
     return(
-        <div>
-            <h1>App</h1>
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className="bg-gray-100">
+                <h1>App</h1>
+            </div>
         </div>
     );
 };
