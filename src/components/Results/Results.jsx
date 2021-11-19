fb9bbbdae441814bd212447850bc92903e8bd8c9
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-19 10:43:12 +0100

    images tab added

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -55,17 +55,10 @@ const Results = () =>{
             );
         case '/videos':
             return (
-                <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-                    {results?.map(({link, title}, index)=>(
-                        <div key={index} className="md:w-2/5 w-full">
-                            <a href={link} target="_blank" rel="noreferrer">
-                                <p className="text-sm">
-                                    {link.length > 30 ? link.substring(0,30) : link}
-                                </p>
-                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
-                                    {title}
-                                </p>
-                            </a>
+                <div className="flex flex-wrap">
+                    {results.map((video, index) => (
+                        <div key={index} className="p-2">
+                            <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px"/>
                         </div>
                     ))}
                 </div>
