48c72b2ef70fafb6e979098a2754ff1b4f7ae598
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-19 10:23:45 +0100

    news tab added

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -55,14 +55,38 @@ const Results = () =>{
             );
         case '/videos':
             return (
-                <div>
-
+                <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+                    {results?.results?.map(({link, title}, index)=>(
+                        <div key={index} className="md:w-2/5 w-full">
+                            <a href={link} target="_blank" rel="noreferrer">
+                                <p className="text-sm">
+                                    {link.length > 30 ? link.substring(0,30) : link}
+                                </p>
+                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                                    {title}
+                                </p>
+                            </a>
+                        </div>
+                    ))}
                 </div>
             );
         case '/news':
             return (
-                <div>
-
+                <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
+                    {results?.entries?.map(({links, id, source, title})=>(
+                        <div key={id} className="md:w-2/5 w-full">
+                            <a href={links?.[0].href} target="_blank" rel="noreferrer" className="hover:underline">
+                                <p className="text-lg  dark:text-blue-300 text-blue-700">
+                                    {title}
+                                </p>
+                                <div className="flex gap-4">
+                                    <a href={source?.href} target="_blank" rel="noreferrer">
+                                        {source?.href}
+                                    </a>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
                 </div>
             );
         default:
