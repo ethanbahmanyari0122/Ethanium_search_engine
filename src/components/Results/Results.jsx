05fdad2a9cbbfdef82c439dc507f5d52d319fb83
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-19 10:19:24 +0100

    videos tab added

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -18,7 +18,7 @@ const Results = () =>{
                 getResults(`/${location.pathname}/q=${searchTerm}&num=40`)
             }
         }
-    },[])
+    },[searchTerm, location.pathname]);
 
     if(isLoading) return <Loading/>;
 
@@ -43,7 +43,7 @@ const Results = () =>{
         case '/images':
             return (
                 <div className="flex flex-wrap justify-center items-center">
-                    {results?.image_results?.map(({image, link})=>(
+                    {results?.image_results?.map(({image, link: {href, title}}, index)=>(
                         <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="noreferrer">
                             <img src={image?.src} alt={title} loading="lazy"/>
                             <p className="w-36 break-words text-sm mt-2">
