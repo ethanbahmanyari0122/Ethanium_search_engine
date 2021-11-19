dc28e52b40a6b8674bf1753cc53039fdfbdcdf35
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-19 09:56:13 +0100

    news title sorted

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -26,6 +26,9 @@ const Results = () =>{
                                 <p className="text-sm">
                                     {link.length > 30 ? link.substring(0,30) : link}
                                 </p>
+                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                                    {title}
+                                </p>
                             </a>
                         </div>
                     ))}
